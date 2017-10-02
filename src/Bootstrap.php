2ce415d1771d59c384e56dd5bd2ed9b268ba5ed3
papparazzo <pappi-@gmx.de>
2017-10-02 23:01:17 +0200

    wip...

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -20,65 +20,55 @@
  *
  */
 
-namespace SFW2;
+namespace SFW2\Core;
 
+use SFW2\Core\Bootstrap\BootstrapException;
 use Dice\Dice;
+use Throwable;
 
 class Bootstrap {
 
-    protected $isDebug = false;
-
-    public function __construct() {
-        if(defined('SFW2')) {
-            throw new Bootstrap\Exception(
-                'Framework allready launched',
-                Bootstrap\Exception::ALLREADY_LAUNCHED
-            );
-        }
-        define('SFW2', true);
-    }
-
-    public function errorHandler($errno, $errstr, $errfile, $errline, $context) {
-        $this->printErrorAndDie($errno, $errstr, $errfile, $errline, debug_backtrace());
-    }
-
-    public function excpetionHandler(\Throwable $e) {
-        $this->printErrorAndDie(0, $e->getMessage(), $e->getFile(), $e->getLine(), $e->getTrace());
-    }
-
-    protected function printErrorAndDie($errno, $errstr, $errfile, $errline, $backTrace) {
-        // FIXME: Check for XML and JSON
-        if(!$this->config->getVal('debug', 'show', false)) {
-            return true;
-        }
-        echo $errno . ': ', $errstr . ' in ' . $errfile . ' on line ' . $errline;
-        echo '<pre>';
-        var_dump($backTrace);
-        echo '</pre>';
-        die();
-    }
-
-
-
-
-
-
-
-    protected $rootPath;
+    /**
+     * @var \Dice\Dice
+     */
+    protected $container;
 
     /**
      * @var \SFW\Config
      */
     protected $config;
 
+    /**
+     * @var string
+     */
+    protected $rootPath;
+
+    public function __construct() {
+        if(defined('SFW2')) {
+            throw new Bootstrap\Exception(
+                'Framework allready launched',
+                BootstrapException::ALLREADY_LAUNCHED
+            );
+        }
+        define('SFW2', true);
+        $this->rootPath = __DIR__;
+    }
 
     public function run($configPath) {
-        $this->config = new Config(
-            $configPath . 'conf.common.php',
-            $this->rootPath . 'SFW/conf.common.php'
-        );
+        $this->container = new Dice;
+        $this->container->addRules([
+            'SFW2\Core\Config' =>
+            [
+                'shared' => true,
+                'constructParams' => [
+                    $configPath . DIRECTORY_SEPARATOR . 'conf.common.php',
+                    $this->rootPath . DIRECTORY_SEPARATOR . 'conf.common.php'
+                ]
+            ]
+        ]);
+        $this->config = $this->container->create('SFW2\Core\Config');
 
-        if($this->config->getVal('debug', 'show', false)) {
+        if($this->config->getVal('debug', 'on', false)) {
             error_reporting(E_ALL);
             ini_set('display_errors', true);
         } else {
@@ -89,10 +79,34 @@ class Bootstrap {
         set_error_handler([$this, 'errorHandler']);
         set_exception_handler([$this, 'excpetionHandler']);
         mb_internal_encoding('UTF-8');
-        ini_set('memory_limit', '256M');
+        ini_set('memory_limit', $this->config->getVal('misc', 'memoryLimit'));
         ini_set(LC_ALL, $this->config->getVal('misc', 'locale'));
         setlocale(LC_TIME, $this->config->getVal('misc', 'locale') . ".UTF-8");
         date_default_timezone_set($this->config->getVal('misc', 'timeZone'));
+
+
+        $this->container->addRules([
+            'SFW2\Core\Session' =>
+            [
+                'shared' => true,
+                'constructParams' => [
+                    $_SERVER['SERVER_NAME']
+                ]
+            ]
+        ]);
+
+        $this->container->addRules([
+            'SFW2\Core\Database' =>
+            [
+                'shared' => true,
+                'constructParams' => [
+                    $this->config->getVal('database', 'host'),
+                    $this->config->getVal('database', 'usr'),
+                    $this->config->getVal('database', 'pwd'),
+                    $this->config->getVal('database', 'db'),
+                ]
+            ]
+        ]);
 
         if($this->isOffline()) {
             $handler = new Response\Handler\Offline($this->config);
@@ -100,12 +114,11 @@ class Bootstrap {
             return;
         }
 
-
         $ctrlConf = $configPath . 'conf.controller.php';
         if(!is_file($ctrlConf)) {
-            throw new Bootstrap\Exception(
-                'File <' . $ctrlConf . '> does not exist',
-                Bootstrap\Exception::CONTROLLER_ARRAY_NOT_SET
+            throw new BootstrapException(
+                'File "' . $ctrlConf . '" does not exist',
+                BootstrapException::CONTROLLER_ARRAY_NOT_SET
             );
         }
 
@@ -136,6 +149,39 @@ class Bootstrap {
 
     }
 
+    public function errorHandler($errno, $errstr, $errfile, $errline, $context) {
+        $this->printErrorAndDie($errno, $errstr, $errfile, $errline, debug_backtrace());
+    }
+
+    public function excpetionHandler(Throwable $e) {
+        $this->printErrorAndDie(0, $e->getMessage(), $e->getFile(), $e->getLine(), $e->getTrace());
+    }
+
+    protected function printErrorAndDie($errno, $errstr, $errfile, $errline, $backTrace) {
+        // FIXME: Check for XML and JSON
+        if(!$this->config->getVal('debug', 'on', false)) {
+            return true;
+        }
+        echo $errno . ': ', $errstr . ' in ' . $errfile . ' on line ' . $errline;
+        echo '<pre>';
+        var_dump($backTrace);
+        echo '</pre>';
+        die();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     protected function isOffline() {
         if(!$this->config->getVal('site', 'offline')) {
             return false;
@@ -146,7 +192,7 @@ class Bootstrap {
         }
 
         if(isset($_GET['bypass']) && $_GET['bypass'] == $this->config->getVal('site', 'offlineBypassToken')) {
-            $session->set('bypass') = true;
+            $session->set('bypass', true);
             return false;
         }
         return true;
