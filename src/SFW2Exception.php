95fc5d11e61c5894a2e1ff9e90814978517a7c2d
4paproth <stefan.paproth@4com.de>
2022-07-25 19:20:30 +0200

    SFW2Exception added

---
+++ src/SFW2Exception.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ *  SFW2 - SimpleFrameWork
+ *
+ *  Copyright (C) 2017  Stefan Paproth
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ *
+ */
+
+namespace SFW2\Core;
+
+use Exception;
+use Throwable;
+
+class SFW2Exception extends Exception {
+    public const UNKNOWN = 0;
+
+    protected readonly string $timeStamp;
+    protected readonly string $identifier;
+
+    public function __construct(string $msg, int $code = self::UNKNOWN, Throwable $prev = null) {
+        $this->timeStamp = time();
+        $this->identifier = strtoupper(md5(microtime() . mt_rand()));
+        parent::__construct($msg, $code, $prev);
+    }
+
+    /**
+     * @return string
+     * @noinspection PhpUnused
+     */
+    public function getTimeStamp(): string {
+        return $this->timeStamp;
+    }
+
+    /**
+     * @return string
+     * @noinspection PhpUnused
+     */
+    public function getIdentifier(): string {
+        return $this->identifier;
+    }
+}
